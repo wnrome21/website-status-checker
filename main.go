b4866e7f3695866641b8d7773d7eb0d31a4bf06d
wnrome21 <nateromero001@gmail.com>
2022-12-13 12:11:02 -0800

    function setup

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	
+	for _, link := range links {
+		
+	}
 }
