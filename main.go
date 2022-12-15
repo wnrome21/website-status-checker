f23e5b33f8568e36e6794e6ae022f90df9bfad57
wnrome21 <nateromero001@gmail.com>
2022-12-15 15:38:04 -0800

    added channel and channel listener to checklink func and main func

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	links := []string{
@@ -10,8 +13,23 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
+
+	c := make(chan string)
 	
 	for _, link := range links {
-		
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<- c)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link,  "might be down!")
+		c <- "Might be down, possibly"
+		return 
+	}
+	fmt.Println(link, "is up!")
+	c <- "Site is up"
 }
