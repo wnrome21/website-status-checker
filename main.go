884a8f70efc797663aefb3cfde9759a9bcce5ccf
wnrome21 <nateromero001@gmail.com>
2022-12-15 16:33:59 -0800

    repeating routines

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -29,9 +29,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down, possibly"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Site is up"
+	c <- link
 }
